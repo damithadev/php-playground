c80189178589d240d7d27efd75c24c6a403af0d3
Damitha <hellodamitha@gmail.com>
2024-02-15 15:11:07 +0530

    first commit

---
+++ googleMap.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div id="googleMap" style="width: 100%; height: 400px"></div>
+<script>
+      function myMap() {
+        var mapProp = {
+          center: new google.maps.LatLng(6.8522, 79.9322), // NSBM Green University coordinates (latitude, longitude)
+          zoom: 15,
+        };
+        var map = new google.maps.Map(
+          document.getElementById("googleMap"),
+          mapProp
+        );
+
+        // Add marker
+        var marker = new google.maps.Marker({
+          position: { lat: 6.8522, lng: 79.9322 }, // NSBM Green University coordinates (latitude, longitude)
+          map: map,
+          title: "NSBM Green University, Sri Lanka",
+          animation:google.maps.Animation.BOUNCE
+        });
+
+
+
+      var marker = new google.maps.Marker({
+      position:myCenter,
+      
+      });
+
+      marker.setMap(map);
+    }
+    </script>
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7ngQlbbaL_qjsvJQp02PFTXc_gO916s8&callback=myMap"></script></body>
+</html>
---
+++ setCookie.php
@@ -0,0 +1,28 @@
+<?php
+$cookie_name = "user";
+$cookie_value = "Damitha Jayatharaka";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30*12), "/"); //cookie will delete within a month.
+// 86400 = 1 day
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+<?php
+if(!isset($_COOKIE[$cookie_name])) {
+  echo "Cookie named '" . $cookie_name . "' is not set!";
+} else {
+  echo "Cookie '" . $cookie_name . "' is set!<br>";
+  echo "Value is: " . $_COOKIE[$cookie_name];
+}
+?>
+</body>
+</html>
